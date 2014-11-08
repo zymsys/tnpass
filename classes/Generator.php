8a3242de8de29b1803ce9dc31003952e3963fd98
Vic Metcalfe <vic@acm.org>
2014-11-07 20:11:15 -0500

    Stubbed generator

---
+++ classes/Generator.php
@@ -0,0 +1,10 @@
+<?php
+namespace TrueNorth\Password;
+
+class Generator
+{
+    public function generate($forSite)
+    {
+        return "secret";
+    }
+}
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once "vendor/autoload.php";
+
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig
+));
+
+$app->get('/', function() use ($app) {
+    $app->render('index.html.twig');
+});
+
+$app->post('/', function () use ($app) {
+    $service = new \TrueNorth\Password\Generator();
+    $app->render('password.html.twig', [
+        'secret' => $service->generate($app->request->post('site'))
+    ]);
+});
+
+$app->run();
